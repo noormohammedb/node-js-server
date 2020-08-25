149e94c80c8d17cf38d8bf0cea2b3f3cac1b1b44
noormohammedb <nooru.mohad@gmail.com>
2020-08-25 10:48:13 +0530

    server code optimized using es6

--- sample-server.js
+++ sample-server.js
@@ -1,9 +1,7 @@
 const http = require('http');
 
-http.createServer(server).listen(3000);
-
-function server(req,res){
+http.createServer((req,res)=>{
     console.log("reqursted : " + req.connection.remoteAddress)
     res.write("Hello World!");
     res.end();
-}
+}).listen(3000);
