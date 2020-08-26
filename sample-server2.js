d339f9d030825a809a7233ced0b4ed67cfc7f319
noormohammedb <nooru.mohad@gmail.com>
2020-08-26 13:55:25 +0530

    serving html file

---
+++ sample-server2.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const fs = require("fs");
+const port = 3000;
+http.createServer((req,res)=>{
+    let page = fs.readFile("index.html",(err,data)=>{
+        console.log("Reqursting " + req.connection.remoteAddress);
+        res.writeHead(200,{"Content-Type":"text/html"});
+        res.write(data);
+        res.end();
+    });
+}).listen(port,()=>{
+    console.log("Server is running at : " + port);
+});
