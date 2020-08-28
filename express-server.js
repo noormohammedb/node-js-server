27afb355a757eba4927d77cb99362d68d1c14932
noormohammedb <nooru.mohad@gmail.com>
2020-08-28 19:04:13 +0530

    path updated

--- express-server.js
+++ express-server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.get('/',(req,res) => {
-    res.sendFile(__dirname+"/index.html");
+    res.sendFile(path.join("/index.html"));
 })
 
 app.get('/login',(req,res) => {
