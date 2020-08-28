974c1b0cb0fb89b534be613957fdbcb116580e24
noormohammedb <nooru.mohad@gmail.com>
2020-08-28 15:31:06 +0530

    rewrite

--- server-html.js
+++ server-html.js
@@ -7,22 +7,19 @@ let server = http.createServer((req,res) => {
     console.log(req.connection.remoteAddress);
     console.log(req.url);
     let reqrl = url.parse(req.url,true);
-    console.log(reqrl.query.who)
-    console.log(reqrl.query.message)
-    // console.log(reqrl);
-    console.log(reqrl.path);
-    if(reqrl.pathname=='/'){   
+    console.log(reqrl.pathname);
+    if(reqrl.pathname == '/'){
         fs.readFile('index.html',(error,data) => {
             res.writeHead(200,{'content-type':'text/html'});
             res.write(data);
-            res.end()
-        })
-    }else if(reqrl.pathname=="/login"){
+            res.end();
+        });
+    }else if(reqrl.pathname == '/login'){
         fs.readFile('login.html',(error,data) => {
             res.writeHead(200,{'content-type':'text/html'});
             res.write(data);
             res.end();
-        })
+        });
     }else{
         res.writeHead(404);
         res.end("Error");
@@ -30,5 +27,5 @@ let server = http.createServer((req,res) => {
 });
 
 server.listen(port,() => {
-    console.log('server listening at port : ' + port);
+    console.log("Server listening at : " + port);
 });
