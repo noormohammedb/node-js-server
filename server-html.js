f81baa4b5458c568ecc848e629b096a4e0de1e8e
noormohammedb <nooru.mohad@gmail.com>
2020-08-28 11:49:31 +0530

    routing

--- server-html.js
+++ server-html.js
@@ -3,13 +3,20 @@ const fs = require('fs');
 const port = 3000;
 
 let server = http.createServer((req,res) => {
+    console.log(req.connection.remoteAddress);
     console.log(req.url);
-    fs.readFile('index.html',(error,data) => {
-        console.log(req.connection.remoteAddress);
-        res.writeHead(200,{'content-type':'text/html'});
-        res.write(data);
-        res.end()
-    })
+    if(req.url=='/'){   
+        fs.readFile('index.html',(error,data) => {
+            res.writeHead(200,{'content-type':'text/html'});
+            res.write(data);
+            res.end()
+        })
+    }else if(req.url==="/login"){
+        res.end("Login");
+    }else{
+        res.writeHead(404);
+        res.end("Error");
+    }
 });
 
 server.listen(port,() => {
