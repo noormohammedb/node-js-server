ef09e5aca33c4158fbafb7d6c89690c47c17a739
noormohammedb <nooru.mohad@gmail.com>
2020-08-28 14:25:48 +0530

    routing updated or optimized

--- server-html.js
+++ server-html.js
@@ -1,18 +1,26 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 const port = 3000;
 
 let server = http.createServer((req,res) => {
     console.log(req.connection.remoteAddress);
     console.log(req.url);
-    if(req.url=='/'){   
+    let req_url = url.parse(req.url);
+    // console.log(req_url);
+    console.log(req_url.path);
+    if(req_url.pathname=='/'){   
         fs.readFile('index.html',(error,data) => {
             res.writeHead(200,{'content-type':'text/html'});
             res.write(data);
             res.end()
         })
-    }else if(req.url==="/login"){
-        res.end("Login");
+    }else if(req_url.pathname=="/login"){
+        fs.readFile('login.html',(error,data) => {
+            res.writeHead(200,{'content-type':'text/html'});
+            res.write(data);
+            res.end();
+        })
     }else{
         res.writeHead(404);
         res.end("Error");
