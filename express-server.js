ddf3abc2c9437f865da2eabae80573e9a68e7cdd
noormohammedb <nooru.mohad@gmail.com>
2020-08-29 13:55:39 +0530

    middleware

--- express-server.js
+++ express-server.js
@@ -4,13 +4,15 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res) => {
+app.use((req,res,next) => {
     console.log(req.connection.remoteAddress);
+    next();
+})
+
+app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'index.html'));
 });
 app.get('/login',(req,res) => {
-    console.log(req.connection.remoteAddress);
-    // console.log(req.);
     res.sendFile(path.join(__dirname,'login.html'));
 });
 
